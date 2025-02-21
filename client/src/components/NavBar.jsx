5f7ca4eb6e46e63becce08f2066cf63f6c3618e3
Mariusflores <mada060@student.kristiania.no>
2025-02-21 12:16:04 +0100

    updated design

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -8,44 +8,46 @@ export function NavBar({ user, onLogout }) {
   return (
     <header id="navbar">
       <div className="link-container">
-        <Link className={'link'} to="/home">
-          Home
-        </Link>
         <span className="icon">
           <FontAwesomeIcon icon={faHouse} size={'0.5'} />
         </span>
+        <Link className={'link'} to="/home">
+          Home
+        </Link>
       </div>
 
       {user ? (
         <>
           <div className="link-container">
-            <Link className={'link'} to={'/publish'}>
-              Post
-            </Link>
             <span className="icon">
               <FontAwesomeIcon icon={faPen} size={'0.5'} />
             </span>
+            <Link className={'link'} to={'/publish'}>
+              Post
+            </Link>
           </div>
           <div className="link-container">
-            <Link className={'link'} to="/profile">
-              Profile
-            </Link>
             <span className="icon">
               <FontAwesomeIcon icon={faUser} size={'0.5'} />
             </span>
+            <Link className={'link'} to="/profile">
+              Profile
+            </Link>
           </div>
           <div className="link">
             <button onClick={onLogout}>Logout</button>
           </div>
         </>
       ) : (
+
         <div className="link-container">
-          <Link className={'link'} to="/login">
-            Login
-          </Link>
           <span className="icon">
             <FontAwesomeIcon icon={faRightToBracket} size={'0.5'} />
           </span>
+          <Link className={'link'} to="/login">
+            Login
+          </Link>
+
         </div>
       )}
     </header>
